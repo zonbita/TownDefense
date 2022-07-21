879ddf585f4c4fb0570d3aa5d7e42d42da6a2cd4
zonbita <50777150+zonbita@users.noreply.github.com>
2022-07-21 12:30:51 +0700

    character

---
+++ Character/GiveExpWhenDie.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GiveExpWhenDie : MonoBehaviour
+{
+    public int expMin = 5;
+    public int expMax = 7;
+
+    public void GiveExp()
+    {
+        int random = Random.Range(expMin, expMax);
+        GameManager.Instance.AddExp(random, transform);
+    }
+}
